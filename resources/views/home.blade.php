e687e8701fede67b181c6a167ec1bb53e2e5f5a6
PongsakDev <xox.nui@gmail.com>
2017-03-09 02:17:17 +0700

    add avatar image

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,9 +5,14 @@
     <div class="row">
 
         <div class="col-md-3">
-            <h4>{{ Auth::user()->name }} {{ Auth::user()->surname }}</h4>
+        <img src="image/avatar/avatar/{{Auth::user()->id}}/avatar.jpeg" class="img-responsive">
+        <a href="/profile/change">Change Profile</a>
+            <h4>{{Auth::user()->name}} {{Auth::user()->surname}} ({{ Auth::user()->nickname }})</h4>
             <div class="list-group">
-              <p class="list-group-item-text">Educaion : Computer En</p>
+              <p class="list-group-item-text"><strong>Education : </strong>{{Auth::user()->education}}</p>
+              <p class="list-group-item-text"><strong>Biography : </strong>{{Auth::user()->biography}}</p>
+              <p class="list-group-item-text"><strong>County : </strong>{{Auth::user()->county}}</p>
+              <p class="list-group-item-text"><strong>Website : </strong> <a href="https://www.Google.com">{{Auth::user()->website}}</a></p>
             </div>
         </div>
 
@@ -15,16 +20,13 @@
             {{-- NewPost --}}
            <div class="panel panel-default">
                <div class="panel-heading">
-                  <div class="panel-body">
-                    <form class="/post/new" method="post" enctype="multipart/form-data">
-                      <div class="form-group">
-                        <textarea class="form-control" name="body" cols="30" rows="8"></textarea>
-                      </div>
-                      <div class="form-group pull-right">
-                        <input class="btn btn-primary" type="submit" value="Post">
-                      </div>    
-                    </form>
-                  </div>
+                  <h3 class="panel-title">New Post</h3>
+                </div>
+                <div class="panel-body">
+                  <form action="/post/new" method="post" enctype="multipart/form-data">
+                    <div class="form-group"><textarea class="form-control" name="body" cols="30" rows="8"></textarea></div>
+                    <div class="form-group pull-right">{{csrf_field()}}<input class="btn btn-primary" type="submit" value="Post"></div>    
+                  </form>
                 </div>
             </div>
 
@@ -32,7 +34,12 @@
             @foreach($posts->get() as $post)
               <div class="panel panel-default">
                 <div class="panel-body">
-                  <h4>{{$post->owner()->first()->name}} {{$post->owner()->first()->surname}}</h4>
+                  <h4>{{$post->owner()->first()->name}} {{$post->owner()->first()->surname}}
+                    <br>
+                    {{-- <small>{{$post->created_at}}</small> --}}
+                    {{-- <small>{{date_format(date_create($post->created_at), "Y/m/d H:i:s")}}</small> --}}
+                    <small>{{date_format(date_create($post->created_at), "Y/m/d")}}</small>
+                  </h4>
                   <p>{{$post->body}}</p>
                   <a href="/like/{{$post->id}}"><strong>{{$post->likes}}Like(s)</strong></a>
                 </div>
