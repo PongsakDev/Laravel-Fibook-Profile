b25aba98bed2805ae23e3ebf6745925243f91baa
PongsakDev <xox.nui@gmail.com>
2017-03-08 22:57:01 +0700

    add new post

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Post;
+
+class PostController extends Controller
+{
+    public function like ($id) {
+        $post = Post::find($id);
+        $post->likes = $post->likes + 1;
+        
+        $post->save();
+        return back();
+    }
+
+    public function post (Request $request) {
+        $post = new Post();
+        $post->body = $request->body;
+        $post->ownerID = Auth::user()->id;
+        
+        $post->save();
+        return back();
+    }
+}
